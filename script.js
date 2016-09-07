25fe836f25ff8cd1249fbc65a959c638f4ac72b6
General Assembly <generalassembly@Generals-MacBook-Air-7.local>
2016-09-07 17:23:27 -0400

    Basic look/function done.

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-      anchors:['welcome', 'about', 'projects', 'contact'],
-      navigaton: false,
-      slidesNaviagion: true,
-      slidesNavPosition: 'bottom',
-      sectionsColor: ['#a09be7', '#5f00ba', '#d6ff79', '#b0ff92', '#fff2f1']
+      anchors:['welcome', 'about', 'projects', 'contact', 'json'],
+      menu: '#myMenu',
+      sectionsColor: ['#847577', '#a6a2a2', '#cfd2cd', '#e5e6e4', '#fbfbf2']
     });
 });
