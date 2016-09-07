b1eca8d21d174830264cfb101c2fbb247635dfa2
General Assembly <generalassembly@Generals-MacBook-Air-7.local>
2016-09-07 15:57:38 -0400

    Fixed css link. Slides are working.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage();
+    $('#fullpage').fullpage({
+      anchors:['welcome', 'about', 'projects', 'contact'],
+      navigaton: false,
+      slidesNaviagion: true,
+      slidesNavPosition: 'bottom',
+      sectionsColor: ['#a09be7', '#5f00ba', '#d6ff79', '#b0ff92', '#fff2f1']
+    });
 });
