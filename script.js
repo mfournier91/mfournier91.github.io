4fcb2e39ffd981fe30c76ad4fd93782fd21d54a5
General Assembly <generalassembly@Generals-MacBook-Air-7.local>
2016-09-21 12:27:14 -0400

    Hover works.

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ $(document).ready(function() {
       menu: '#myMenu',
       sectionsColor: ['#847577', '#a6a2a2', '#cfd2cd', '#e5e6e4', '#fbfbf2']
     });
+
+    $('.hoverable').hover(hoverStart, hoverEnd);
+
+    function hoverStart() {
+      $(this).addClass("hovered");
+      var currentClasses = $(this).attr('class');
+    }
+
+    function hoverEnd() {
+      $(this).removeClass("hovered")
+      var currentClasses = $(this).attr('class');
+    }
 });
